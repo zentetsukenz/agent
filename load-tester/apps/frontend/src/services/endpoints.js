b3720d942e38e485d4a268e3fe6cacc8291efef0
Wiwatta Mongkhonchit <zentetsukenz@gmail.com>
2026-01-05 23:27:41 +0700

    Move load-tester out

--- load-tester/apps/frontend/src/services/endpoints.js
+++
@@ -1,9 +0,0 @@
-import { api } from "./api";
-
-export const endpointsAPI = {
-  getAll: () => api.get("/endpoints"),
-  getById: (id) => api.get(`/endpoints/${id}`),
-  create: (data) => api.post("/endpoints", data),
-  update: (id, data) => api.put(`/endpoints/${id}`, data),
-  delete: (id) => api.delete(`/endpoints/${id}`),
-};
